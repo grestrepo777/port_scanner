669f4f019a6146eda2ae1d2da16366ed7a413a8b
grestrepo777 <122930865+grestrepo777@users.noreply.github.com>
2025-06-07 21:29:33 -0400

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,17 @@
+import socket, sys
+
+try: 
+    target = input("Enter IP to scan: ")
+    print(f"\nScanning {target} from port 1 to port 1024\n")
+
+    for port in range (20, 1024):  # port range 1 - 1024
+        openSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create new socket object (IPv4, TCP)
+        openSocket.settimeout(0.2) # give it some time to wait for a response
+        result = openSocket.connect_ex((target, port))  # Returns a value based on whether a port is open or closed
+        if result == 0: # 0 = open port, any other value = closed port
+            print(f"Port {port} is open")
+        openSocket.close() # close the socket/end connection
+
+except KeyboardInterrupt:  # ctrl + c to end the scan
+    print("Scanning cancelled")
+    sys.exit()
