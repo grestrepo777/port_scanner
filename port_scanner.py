3673f11157b623edbeb7ace08db75b4f324d9a7c
grestrepo777 <122930865+grestrepo777@users.noreply.github.com>
2025-09-14 16:45:35 -0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,7 +2,7 @@ import socket, sys
 
 try: 
     target = input("Enter IP to scan: ")
-    print(f"\nScanning {target} from port 1 to port 1024\n")
+    print(f"\nScanning {target} from port 20 to port 1023\n")
 
     for port in range (20, 1024):  # port range 20 - 1023
         openSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create new socket object (IPv4, TCP)
@@ -16,3 +16,4 @@ except KeyboardInterrupt:  # ctrl + c to end the scan
     print("Scanning cancelled")
 
     sys.exit()
+
